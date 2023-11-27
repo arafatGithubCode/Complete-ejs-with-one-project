bce557e7c9b2c2749c681e9c5cf5c8276c96f1fb
arafatGithubCode <md489288@gmail.com>
2023-11-27 10:30:40 +0600

    initial setup and passing data to ejs file

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+const pLanguages = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { plName: pLanguages });
+});
+
+app.post("/", (req, res) => {
+  const pLang = req.body.pLang;
+  pLanguages.push(pLang);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
